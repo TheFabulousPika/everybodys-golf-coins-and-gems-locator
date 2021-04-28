ba7136941521264cb80d307211616457668f26ed
TheFabulousPika <65629794+TheFabulousPika@users.noreply.github.com>
2021-04-28 15:01:55 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-function days_passed() {
-    var oneDay = 24*60*60*1000; // hours*minutes*seconds*milliseconds
-    var firstDateUTC = new Date(Date.UTC(2019,08,11));
-    var secondDate = new Date();
-    var diffDays = Math.floor(Math.abs((firstDateUTC.getTime() - secondDate.getTime())/(oneDay)));
+function getDaysPassed() {
+    const oneDayInMilliseconds = 24*60*60*1000; // hours*minutes*seconds*milliseconds
+    let firstDateUTC = new Date(Date.UTC(2019,08,11));
+    let currentDate = new Date();
+    let diffDays = Math.floor(Math.abs((firstDateUTC.getTime() - currentDate.getTime())/(oneDayInMilliseconds)));
     return diffDays;
 }
-function getEagleOutHoleNumber(){
-    var daysPassed = days_passed();
-    var remainder = daysPassed % 9;
-    var EagleOutHoleNumber = 4 + remainder;
+function getEagleCityOutItemHoleNumber(){
+    let daysPassed = getDaysPassed();
+    let remainder = daysPassed % 9;
+    let EagleOutHoleNumber = 4 + remainder;
     return EagleOutHoleNumber;
 }
 function initializeTable(){
-    addIcons();
+    addIconsToTable();
     updateGolfIslandTime();
     updateItemHolesInTable();
 }
@@ -21,11 +21,11 @@ function updateTable(){
     updateItemHolesInTable();
 }  
 function updateItemHolesInTable(){
-    var EagleOutHoleNumber = Number(getEagleOutHoleNumber());
-    var tdOut = document.getElementsByClassName("out");
-    var tdIn = document.getElementsByClassName("in");
+    let eagleCityOutItemHoleNumber = Number(getEagleCityOutItemHoleNumber());
+    let tdOut = document.getElementsByClassName("out");
+    let tdIn = document.getElementsByClassName("in");
     for (var i = 0; i < tdOut.length; i++){
-        var thisOutHoleNumber = (EagleOutHoleNumber + i*2) % 9;
+        let thisOutHoleNumber = (eagleCityOutItemHoleNumber + i*2) % 9;
         if (thisOutHoleNumber == 0){
             thisOutHoleNumber = 9;
         }
@@ -38,17 +38,17 @@ function updateItemHolesInTable(){
     }  
 }
 function updateGolfIslandTime(){
-    var tdGolfIslandTime = document.getElementById("islandtime");
-    tdGolfIslandTime.innerHTML = golfIslandTime();
+    let tdGolfIslandTime = document.getElementById("islandtime");
+    tdGolfIslandTime.innerHTML = getGolfIslandTime();
 }
-function golfIslandTime(){
-    var golfIslandTime = new Date().toUTCString();
+function getGolfIslandTime(){
+    let golfIslandTime = new Date().toUTCString();
     return golfIslandTime;
 }
-function addIcons(){
-    var tdIcon = document.getElementsByClassName("icon");
+function addIconsToTable(){
+    let tdIcon = document.getElementsByClassName("icon");
     for (var i = 0; i < tdIcon.length; i++){
-        var bgURL = "url(" + "'icons/" + i + ".png')";
+        let bgURL = "url(" + "'icons/" + i + ".png')";
         tdIcon[i].style.backgroundImage = bgURL;
     }
 }
