428a0bf10baa906cb6fa8efe4ca5ced3afd1e6a3
TheFabulousPika <65629794+TheFabulousPika@users.noreply.github.com>
2021-05-02 17:43:33 -0700

    Create data.js

---
+++ data.js
@@ -0,0 +1,4 @@
+const monthChecked = 5; //Month 1-12
+const dateChecked = 2; //Date 1-31
+const yearChecked = 2021; //Year
+const eagleCityOutItemHoleNum = 4; //Item hole number in Eagle City Out map. Possible valuess 1-9
