dcfd4873887adbf5f1ed18d0c2d0e7445ba099d6
TheFabulousPika <65629794+TheFabulousPika@users.noreply.github.com>
2021-05-02 21:39:08 -0700

    Update data.js

--- data.js
+++ data.js
@@ -1,4 +1,7 @@
-const monthChecked = 5; //Month 1-12
+//To resynchronize, check item hole number in Out map of Eagle City GC
+//Update the following lines
+//Date should be the date in Gold Island Time
+const monthChecked = 5; //Month 1-12; Enter as-is (1 for Jan, 2 for Feb, and so forth) without accounting for Javascript date's month offset.
 const dateChecked = 2; //Date 1-31
 const yearChecked = 2021; //Year
 const eagleCityOutItemHoleNum = 4; //Item hole number in Eagle City Out map. Possible valuess 1-9
