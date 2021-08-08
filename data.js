35ba28e4bb128bd582e83365af84331f021d449e
TheFabulousPika <65629794+TheFabulousPika@users.noreply.github.com>
2021-08-08 01:18:36 -0700

    Update data.js

--- data.js
+++ data.js
@@ -1,7 +1,7 @@
 //To resynchronize, check item hole number in Out map of Eagle City GC
 //Update the following lines
 //Date should be the date in Golf Island Time
-const monthChecked = 5; //Month 1-12; Enter as-is (1 for Jan, 2 for Feb, and so forth) without accounting for Javascript date's month offset.
-const dateChecked = 2; //Date 1-31
+const monthChecked = 8; //Month 1-12; Enter as-is (1 for Jan, 2 for Feb, and so forth) without accounting for Javascript date's month offset.
+const dateChecked = 8; //Date 1-31
 const yearChecked = 2021; //Year
-const eagleCityOutItemHoleNum = 4; //Item hole number in Eagle City Out map. Possible valuess 1-9
+const eagleCityOutItemHoleNum = 1; //Item hole number in Eagle City Out map. Possible valuess 1-9
